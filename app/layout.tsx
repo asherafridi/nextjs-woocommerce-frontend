4ee2e4345f8623e125afe177cf1a9eeecf0d681d
Ashir Afridi <ashirafridi.work@gmail.com>
2025-10-23 01:52:54 +0500

    initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  variable: "--font-dm-sans",
+  weight: ["400", "500", "700"], // optional: choose weights you’ll use
+});
+
+export const metadata: Metadata = {
+  title: "Zain Mart",
+  description: "Building for the web.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={dmSans.variable}>
+      <body className="font-sans antialiased">
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <section className='bg-gray-800 text-white'>
+        <div className='container mx-auto py-2 flex justify-between items-center'>
+            <p className='text-sm'>Get Free Delivery on orders over 5000 Rs</p>
+            <p className='text-sm'>Welcome Back</p>
+        </div>
+    </section>
+  )
+}
+
+export default Header
