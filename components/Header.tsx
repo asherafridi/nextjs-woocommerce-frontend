6ea6a21bab42e83d2ddbe4f8c4c4c55ae6f97b13
Ashir Afridi <ashirafridi.work@gmail.com>
2025-10-23 05:45:10 +0500

    dark issue resolved

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <section className='bg-gray-800 text-white'>
+    <section className='hidden md:block bg-gray-800 text-white'>
         <div className='container mx-auto px-6 py-2 flex justify-between items-center'>
             <p className='text-sm'>Get Free Delivery on orders over 5000 Rs</p>
             <p className='text-sm'>Welcome Back</p>
