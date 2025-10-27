eb5611a63b3727ab3c244bde22583eddc85bbf68
Ashir Afridi <ashirafridi.work@gmail.com>
2025-10-27 17:13:12 +0500

    checking product slug

--- app/(pages)/products/[slug]/page.tsx
+++ app/(pages)/products/[slug]/page.tsx
@@ -1,30 +1,11 @@
-import { wcApi } from "@/lib/woocommerce";
-import axios from "axios";
-import Image from "next/image";
-import React, { use } from "react";
-import { useRouter } from "next/navigation";
+import React from 'react'
 
+const page = async ({params}:{params: Promise<{slug:string}>}) => {
 
-// Fetch single product by slug
-async function getProductBySlug(slug: string) {
-  const { data } = await axios.get(`http://localhost:3000/api/products/get?slug=${slug}`);
-  return data;
+    const slug = (await params).slug;
+  return (
+    <div>{slug}</div>
+  )
 }
 
-const page = ({slug}:{slug:string}) => {
-     
-  const product = getProductBySlug(slug as string);
-  console.log(product);
-
-  if (!product) {
-    return <div className="text-center py-10 text-gray-600">Product not found</div>;
-  }
-
-  return (
-    <div className="max-w-5xl mx-auto p-6">
-      {slug}
-    </div>
-  );
-};
-
-export default page;
+export default page
