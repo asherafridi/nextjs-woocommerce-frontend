ff01a267f7405d39b990f1e76687d8d09e7845bc
Ashir Afridi <ashirafridi.work@gmail.com>
2025-10-27 10:43:17 +0500

    remote patterns added

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ["slateblue-sheep-666523.hostingersite.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "slateblue-sheep-666523.hostingersite.com",
+        port: "",
+        pathname: "/wp-content/uploads/**", // allow all images under this path
+      },
+    ],
   },
 };
 
