0ec363bfc11d268eee47966592e2c69adaa0a223
Ashir Afridi <ashirafridi.work@gmail.com>
2025-10-27 12:29:11 +0500

    remote patterns added

--- app/(pages)/products/[slug]/page.tsx
+++ app/(pages)/products/[slug]/page.tsx
@@ -2,7 +2,15 @@ import { wcApi } from "@/lib/woocommerce";
 import Image from "next/image";
 import React from "react";
 
-
+async function getProductBySlug(slug: string) {
+  try {
+    const { data } = await wcApi.get("products", { slug });
+    return data[0];
+  } catch (error) {
+    console.error("Error fetching product:", error);
+    return null;
+  }
+}
 
 
 // ✅ Fix: tell Next.js to treat this as a normal async component
@@ -13,6 +21,10 @@ const ProductPage = async ({
 }) => {
   const { slug } = await params;
 
+  
+    const { data } = await wcApi.get("products", { slug });
+
+    console.log("Product data:", data);
 
   return (
     <div className="max-w-5xl mx-auto p-6">
